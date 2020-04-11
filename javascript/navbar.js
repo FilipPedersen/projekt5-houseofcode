cb3986a4f5c47391a800cf8eb9ac721da1e84ecd
fili4193 <60428645+fili4193@users.noreply.github.com>
2020-04-11 17:56:24 +0200

    indsat fungerende navbar

---
+++ javascript/navbar.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+  const burgermenu = document.querySelector('.menu');
+  const nav = document.querySelector('.nav-links2');
+
+  burgermenu.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+  });
+}
+
+navSlide();
