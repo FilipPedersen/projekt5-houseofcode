b1a27cc860b9d5fb94d33d64205d9bbcf2d36b3e
fili4193 <60428645+fili4193@users.noreply.github.com>
2020-04-14 12:52:12 +0200

    js til kontaktformularen (virker til mobilversion)

---
+++ javascript/kontakt.js
@@ -0,0 +1,30 @@
+function validation(){
+  var name = document.getElementById("name").value;
+  var nummer = document.getElementById("nummer").value;
+  var email = document.getElementById("email").value;
+  var error_message = document.getElementById("error_message");
+  var text;
+
+  error_message.style.padding = "10px";
+
+  if(name.length <3){
+    text = "Skriv venligst dit navn";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Skriv venligst en gyldig email";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(isNaN(nummer) || nummer.length !=8){
+    text = "Skriv venligst et gyldigt nummer";
+    error_message.innerHTML = text;
+    return false;
+  }
+  
+  alert("Din besked blev sendt!")
+  return true;
+}
