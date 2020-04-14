3f628b3e4852e51faed955a6df30cdec7160685f
Oliver-Zarp <60459243+Oliver-Zarp@users.noreply.github.com>
2020-04-14 21:06:56 +0200

    mobil ændring 2
    mobil ændring 2

--- javascript/js5.js
+++ javascript/js5.js
@@ -9,23 +9,13 @@ $(document).ready(function(){
                 .css({'background-image': 'url('+newBg+')'})//ændre css bagrrund billede til new bg
                 .animate({opacity: 1}) //sætter opacity til 1 igen
                 .css({'background-image': 'url('+oldBg+')'}) //ændre css bagrrund billede til old bg
-                .load("integrer-hjemmeside.html"); //loader den givne side husk at brug live server
+                .load("logind.html"); //loader den givne side husk at brug live server
 
         });
     });
 
 
-    $(".knapper2").click(function(){ //tjekker når der klikkes på den knap med klassen standardknap
-      $('body') //tager fat i bodyen af den givne html side
-          .animate({opacity: 0},300 , function() { // bruger jquery animate funktion til at ændre opacity og venter 300 del af et sekund
-              $(this)//refererer til min parent altså body
-                  .css({'background-image': 'url('+newBg+')'})//ændre css bagrrund billede til new bg
-                  .animate({opacity: 1}) //sætter opacity til 1 igen
-                  .css({'background-image': 'url('+oldBg+')'}) //ændre css bagrrund billede til old bg
-                  .load("integrer-hjemmeside.html"); //loader den givne side husk at brug live server
 
-          });
-      });
 
 
     $(".mobilknap").click(function(){ //tjekker når der klikkes på den knap med klassen standardknap
