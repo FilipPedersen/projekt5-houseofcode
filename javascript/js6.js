7ada2acd6d2e031df850e0589b174d6dfea6266d
Thorfml <60459871+Thorfml@users.noreply.github.com>
2020-04-14 20:16:48 +0200

    ALLE VIRKER NU

--- javascript/js6.js
+++ javascript/js6.js
@@ -15,6 +15,20 @@ $(document).ready(function(){
     });
 
 
+    $(".knapper2").click(function(){ //tjekker når der klikkes på den knap med klassen standardknap
+      $('body') //tager fat i bodyen af den givne html side
+          .animate({opacity: 0},300 , function() { // bruger jquery animate funktion til at ændre opacity og venter 300 del af et sekund
+              $(this)//refererer til min parent altså body
+                  .css({'background-image': 'url('+newBg+')'})//ændre css bagrrund billede til new bg
+                  .animate({opacity: 1}) //sætter opacity til 1 igen
+                  .css({'background-image': 'url('+oldBg+')'}) //ændre css bagrrund billede til old bg
+                  .load("logind.html"); //loader den givne side husk at brug live server
+
+          });
+      });
+
+
+
 
 
 
