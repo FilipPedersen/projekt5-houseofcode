004c0acfcac5a7401c08ece7569467e5efdd9b44
Thorfml <60459871+Thorfml@users.noreply.github.com>
2020-04-15 22:42:07 +0200

    Styleguide

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,5 @@
-/* Jeg bruger en jQuery her til at skifte stylen af knapper (button) */
-
-/* Jeg laver en click funktion Ved hjælp af jQueryen ($), som hentter
-   alle knapper med id=farve-skift */
   $( "button#farve-skift" ).click(function() {
 
-/* $(this) er en jQuery-indpakning omkring det element, der muliggør brug af
-   jQuery-metoder, som her er .toggleClass.
 
-  .toggleClass add'er eller fjerner en class til et element, her tilfører
-  .toggleClass classen "selected" til den trykket knap*/
   $(this).toggleClass( "selected" );
 });
