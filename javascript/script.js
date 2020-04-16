97d6c1361eaa825c19f210520b66cccb28b3c6e7
Oliver-Zarp <60459243+Oliver-Zarp@users.noreply.github.com>
2020-04-16 03:02:33 +0200

    Rettelser
    rettelser

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,33 @@
-  $("button#farve-skift").click(function() {
+$("button#farve-skift").click(function() {
+
+  $(this).toggleClass("selected");
+});
+
+$("button#farve-skift2").click(function() {
+
+  $(this).toggleClass("selected");
+});
 
 
-    $(this).toggleClass("selected");
-  });
+$("button#farve-skift3").click(function() {
+
+  $(this).toggleClass("selected");
+});
+
+
+$("button#farve-skift4").click(function() {
+
+  $(this).toggleClass("selected");
+});
+
+
+$("button#farve-skift5").click(function() {
+
+  $(this).toggleClass("selected");
+});
+
+
+$("button#farve-skift6").click(function() {
+
+  $(this).toggleClass("selected");
+});
